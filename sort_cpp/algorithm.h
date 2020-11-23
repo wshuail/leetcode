91dc088d9e4fb056174fbc0ab835a97f88cb8f86
wshuail <oopswangsl@gmail.com>
2020-11-23 17:22:15 +0800

    rm cpp sort

--- sort_cpp/algorithm.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SORT__H__
-#define __SORT__H__
-void bubble_sort(int[], int);
-void select_sort(int[], int);
-void insert_sort(int[], int);
-void shell_sort(int[], int);
-void quick_sort(int[], int, int);
-void heap_sort(int[], int);
-#endif
--- sort_cpp/sort_util.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SORT_UTIL_H_
-#define __SORT_UTIL_H_
-
-void random_array(int[], int, int, int);
-void print_array(int[], int);
-bool check_ascending(int[], int);
-bool check_descending(int[], int);
-
-#endif
