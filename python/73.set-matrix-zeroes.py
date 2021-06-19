fd26cee06cd0e4adb85337d073b8f089a8b68121
wshuail <oopswangsl@gmail.com>
2021-06-19 18:49:41 +0800

    update 73

---
+++ python/73.set-matrix-zeroes.py
@@ -0,0 +1,69 @@
+#
+# @lc app=leetcode.cn id=73 lang=python
+#
+# [73] 矩阵置零
+#
+# https://leetcode-cn.com/problems/set-matrix-zeroes/description/
+#
+# algorithms
+# Medium (59.44%)
+# Total Accepted:    111.3K
+# Total Submissions: 187.2K
+# Testcase Example:  '[[1,1,1],[1,0,1],[1,1,1]]'
+#
+# 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+# 
+# 进阶：
+# 
+# 
+# 一个直观的解决方案是使用  O(mn) 的额外空间，但这并不是一个好的解决方案。
+# 一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
+# 你能想出一个仅使用常量空间的解决方案吗？
+# 
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：matrix = [[1,1,1],[1,0,1],[1,1,1]]
+# 输出：[[1,0,1],[0,0,0],[1,0,1]]
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+# 输出：[[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# m == matrix.length
+# n == matrix[0].length
+# 1 
+# -2^31 
+# 
+# 
+#
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        m, n = len(matrix), len(matrix[0])
+        row, col = set(), set()
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    row.add(i)
+                    col.add(j)
+
+        for i in range(m):
+            for j in range(n):
+                if i in row or j in col:
+                    matrix[i][j] = 0
